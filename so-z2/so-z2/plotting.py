413f3c8579b475e93e51776f755ae815d06efbed
Jan Krukowski <jan.krukowski@micazook.com>
2013-11-14 17:43:20 +0100

    minor changes

--- so-z2/so-z2/plotting.py
+++ so-z2/so-z2/plotting.py
@@ -2,10 +2,16 @@ __author__ = 'Janek Krukowski'
 from algo import *
 
 
+def generate_data():
+    return {
+        "hd": Hd(200, arg_start=65),
+        "data": [Proc(100), Proc(198), Proc(44), Proc(132), Proc(2), Proc(134), Proc(70), Proc(72)],
+        "demand": Demand(hd, count=10, arg_data=data)
+    }
+
+
 if __name__ == '__main__':
-    hd = Hd(200, arg_start=65)
-    data = [Proc(100), Proc(198), Proc(44), Proc(132), Proc(2), Proc(134), Proc(70), Proc(72)]
-    demand = Demand(hd, count=10, arg_data=data)
+
     algos = [Fcfs, Ssft, Scan, Cscan]
     for a in algos:
         algo = a(hd, demand)
