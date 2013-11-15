e858c3f5386f3a904b92d5b25f78f87bb2015b0d
Jan Krukowski <jan.krukowski@micazook.com>
2013-11-15 16:15:29 +0100

    generating data changes

--- so-z2/so-z2/plotting.py
+++ so-z2/so-z2/plotting.py
@@ -3,15 +3,16 @@ from algo import *
 
 
 def generate_data():
-    return {
-        "hd": Hd(200, arg_start=65),
-        "data": [Proc(100), Proc(198), Proc(44), Proc(132), Proc(2), Proc(134), Proc(70), Proc(72)],
-        "demand": Demand(hd, count=10, arg_data=data)
-    }
+    hd = Hd(200, arg_start=65)
+    data = [Proc(100), Proc(198), Proc(44), Proc(132), Proc(2), Proc(134), Proc(70), Proc(72)]
+    demand = Demand(hd, count=10, arg_data=data)
+    return hd, data, demand
 
+def generate_data_random():
+    pass
 
 if __name__ == '__main__':
-
+    hd, data, demand = generate_data()
     algos = [Fcfs, Ssft, Scan, Cscan]
     for a in algos:
         algo = a(hd, demand)
