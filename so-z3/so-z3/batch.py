659c6353a226d7d0f2e321aba4713cf39de57f83
Jan Krukowski <jan.krukowski@micazook.com>
2013-11-27 14:38:52 +0100

    added plotting

--- so-z3/so-z3/batch.py
+++ so-z3/so-z3/batch.py
@@ -1,5 +1,7 @@
 __author__ = 'Janek Krukowski'
 from algo import *
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 def get_algos(count):
@@ -19,11 +21,17 @@ def put_data(algos, data):
     return {key: val.swaps for key, val in algos.iteritems()}
 
 
-def run(nmax, count, fcount):
+def batch(nmax, count, fcount):
     data = get_random_calls(nmax=nmax, count=count)
     algos = get_algos(count=fcount)
-    return put_data(algos, data)
+    return put_data(algos, data), data
 
 
 if __name__ == "__main__":
-    print run(nmax=10, count=1000, fcount=4)
+    out_data, in_data = batch(nmax=10, count=1000, fcount=4)
+    sdata, pdata = pd.Series(out_data), pd.Series([i.number for i in in_data])
+    sdata.sort()
+    print sdata
+    print pdata.describe()
+    sdata.plot(kind='barh')
+    plt.show()
