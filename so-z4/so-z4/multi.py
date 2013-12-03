1ec82e399801212bbf5d2ceb2d931259dccf4db8
Janek Lenovo <janekkrukowski@gmail.com>
2013-12-03 22:24:40 +0100

    changes in process creation

--- so-z4/so-z4/multi.py
+++ so-z4/so-z4/multi.py
@@ -42,10 +42,10 @@ class ProcessManager(object):
         self.max_process_calls = max_process_calls
         self.swap_algo = swap_algo
         self.allo_type = allo_type
-        self.proc_set = set()
+        self.proc_set = {self.create_process() for i in xrange(self.process_count)}
 
     def get_process_calls(self):
-        return get_random_calls(r=10, random.randint(self.min_process_calls, self.max_process_calls))
+        return get_random_calls(10, random.randint(self.min_process_calls, self.max_process_calls))
 
     def get_process_frames(self):
         return self.process_frames
@@ -61,5 +61,6 @@ class ProcessManager(object):
         for proc in self.proc_set:
             proc.put()
 
-
-pm = ProcessManager()
+if __name__ == "__main__":
+    pm = ProcessManager()
+    pm.tick()
