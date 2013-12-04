f1e95e7c2b7332918621a95534f36a4d1f1ea868
Janek Lenovo <janekkrukowski@gmail.com>
2013-12-04 21:59:57 +0100

    added function to set frames length when algo is set to prop

--- so-z4/so-z4/multi.py
+++ so-z4/so-z4/multi.py
@@ -2,7 +2,16 @@ __author__ = 'Janek Krukowski'
 
 from algo import *
 import random
+import math
+from numpy import histogram
+from itertools import izip, tee
 
+
+def pairwise(iterable):
+    """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
 
 class Process(Lru):
     def __init__(self, frames_count, calls, allo_type):
@@ -32,10 +41,6 @@ class Process(Lru):
             super(Process, self).put(self.calls[self.pointer])
             self.pointer += 1
 
-    def set_frames(self):
-        "function to chage number of frames"
-        pass
-
     def __repr__(self):
         return "<PROCESS: len={0}, init_len={1}, frames={2}>".format(len(self.calls) - self.pointer - 1,
                                                                      len(self.calls),
@@ -43,8 +48,8 @@ class Process(Lru):
 
 
 class ProcessManager(object):
-    def __init__(self, process_count=2, process_frames=4, system_frames=60,
-                 min_process_calls=500, max_process_calls=5000, allo_type="stable"):
+    def __init__(self, process_count=10, process_frames=4, system_frames=60,
+                 min_process_calls=500, max_process_calls=5000, allo_type="prop"):
         self.process_count = process_count
         self.process_frames = process_frames
         self.system_frames = system_frames
@@ -52,6 +57,26 @@ class ProcessManager(object):
         self.max_process_calls = max_process_calls
         self.allo_type = allo_type
         self.proc_set = {self.create_process() for i in xrange(self.process_count)}
+        if self.allo_type == "prop":
+            self.set_process_frames()
+
+    def set_process_frames(self):
+        data = [proc.size for proc in self.proc_set]
+        k = int(math.sqrt(self.process_count))
+        _, edges = histogram(data, k)
+        groups = []
+        for lower, upper in pairwise(edges):
+            groups.append([p for p in self.proc_set if lower <= p.size <= upper])
+        frames_group = [i+self.process_frames for i in xrange(len(groups))]
+        assiged = 0
+        for gr, fr in izip(groups, frames_group):
+            for p in gr:
+                p.count = fr
+                assiged += fr
+        if self.system_frames < assiged:
+            for i in xrange(self.system_frames - assiged):
+                for proc in self.proc_set:
+                    proc.count -= 1
 
     def create_process(self):
         calls = get_random_calls(4, random.randint(self.min_process_calls, self.max_process_calls))
@@ -61,7 +86,6 @@ class ProcessManager(object):
     def tick(self):
         for proc in self.proc_set:
             proc.put()
-            proc.set_frames()
 
     @property
     def swaps(self):
@@ -82,6 +106,8 @@ if __name__ == "__main__":
     pm = ProcessManager()
     while not pm.finished:
         pm.tick()
-        #print pm
     print pm.swaps
+
+# TODO:
+# 1. remove generating data to separate outer function
 
